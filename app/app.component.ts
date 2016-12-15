85fb5cc68a2e953f8185c5bc24baa5463f4187a6
Aaron Marisi <aarongithub@outlook.com>
2016-12-14 23:02:46 -0500

    Moved template example to app.component.ts

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'my-app',
     moduleId: module.id,
-    template: '<my-rooster-sauce></my-rooster-sauce>',
+    selector: 'my-app',    
+    template: `
+        <input type="text" [value]="food" (input)="foodInput($event)"/>
+        <p>Sriracha sauce is great with {{ food }}</p>
+    `,
 })
-export class AppComponent { }
+export class AppComponent { 
+    food = 'kielbasa';
+
+    foodInput(event: Event) {
+        const target = event.target as HTMLInputElement;
+        this.food = target.value;
+    }
+}
